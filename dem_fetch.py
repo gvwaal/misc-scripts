4a75973ef69e30bf0962834c79ed2cb746c47a9d
Gerrit VanderWaal <gerrit.vwaal@gmail.com>
2017-09-06 14:57:33 -0500

    Added error-proofing
    Checks local URL against remote server. Also made things neater, removed
    excessive text

--- dem_fetch.py
+++ dem_fetch.py
@@ -1,6 +1,6 @@
 # Purpose: a script to download zipped DEMs from URLs in a file for future manipulation.
 #
-# Version 1.0
+# Version 1.1
 #
 # Requires: a pre-exisitng file containing URLs
 #---
@@ -26,19 +26,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #---
-print("importing ftplib...")
+print("importing libraries...")
 import ftplib
-
-print("importing os...")
 import os
-
-print("importing time...\n")
 import time
-
 import zipfile
 
 # Creates file object at a given location with read-only permissions ('r')
-print("opening URL file...\n")
+print("opening URL file...")
 url_file = open(r"C:\Users\Gerrit\GIS\optimal_agate_picking\URLs.txt",'r')
 
 # Sets output directory
@@ -49,43 +44,62 @@ print("connecting to DNR FTP server...")
 ftp_server = ftplib.FTP('ftp.lmic.state.mn.us')
 ftp_server.login()
 
-# Loops through the URLs contained in the file
+# Fetches list (literally) of files from desired directories for later error-proofing
+block1_dir = ftp_server.nlst(r"/pub/data/elevation/lidar/projects/arrowhead/block_1/geodatabase")
+block3_dir = ftp_server.nlst(r"/pub/data/elevation/lidar/projects/arrowhead/block_3/geodatabase")
+
+count = 0
+invalid_url = 0
+
+# Loops through the URLs contained in the local file
 for URL in url_file:
-  # Create new file path to change directories into
-  new_ftp_dir = os.path.join("/pub/data/elevation/lidar/projects/arrowhead/" + URL[71:79] + "geodatabase/")
+  
+    # Create new file path to change directories into, also checks against directories
+    new_ftp_dir = os.path.join("/pub/data/elevation/lidar/projects/arrowhead/" + URL[71:79] + "geodatabase/")
+  
+    # Create filename to fetch
+    new_ftp_dem = URL[91:].rstrip('\r\n')
+  
+    # Creates the full filepath on the remote server based on the local URL file
+    check_file = os.path.join(new_ftp_dir,new_ftp_dem)
+  
+    # Checks whether or not the file actually exists. If it does not, the loop iterates.
+    if (check_file in block1_dir) or (check_file in block3_dir):
+  
+        count += 1
 
-  # Create filename to fetch
-  new_DEM = URL[91:].rstrip('\r\n')
+        # Sets new local file name
+        new_local_dem = os.path.join(output_dir,new_ftp_dem)
 
-  # Sets new local file name
-  new_local = os.path.join(output_dir,new_DEM)
+        # Change into previously created file path
+        ftp_server.cwd(new_ftp_dir)
   
-  # Change into previously created file path
-  print("changing into correct remote directory...")
-  ftp_server.cwd(new_ftp_dir)
-  
-  # Retrieve DEM designated in URL, where the file in newDEM is passed to RETR with the %s placeholder.
-  print("fetching " + new_DEM + ", saving to disk...")
-  ftp_server.retrbinary('RETR %s' % new_DEM, open(new_local, 'wb').write)
+        # Retrieve DEM designated in URL, where the file in newDEM is passed to RETR with the %s placeholder.
+        print("fetching " + new_ftp_dem + ", saving to disk...")
+        ftp_server.retrbinary('RETR %s' % new_ftp_dem, open(new_local_dem, 'wb').write)
 
-  # Returns to base directory
-  print("returning to base directory...")
-  ftp_server.cwd('/pub/data/elevation/lidar/projects/arrowhead/')
+        # Returns to base directory
+        ftp_server.cwd('/pub/data/elevation/lidar/projects/arrowhead/')
   
-  # Creates a zipfile object, extracts the given file, and closes the object
-  print("unzipping...")
-  zip_object = zipfile.ZipFile(new_local, 'r')
-  zip_object.extractall(output_dir)
-  zip_object.close()
-  
-  # Waiting 5 seconds before fetching another file, for the sake of politeness
-  print("waiting 5 seconds...\n")
-  time.sleep(5)
+        # Creates a zipfile object, extracts the given file, and closes the object
+        print("unzipping...")
+        zip_object = zipfile.ZipFile(new_local_dem, 'r')
+        zip_object.extractall(output_dir)
+        zip_object.close()
+
+        # Waiting 5 seconds before fetching another file, for the sake of politeness
+        print("waiting 5 seconds...\n")
+        time.sleep(5)
+
+    else:
+        invalid_url += 1
+
+print("\n" + str(count) + " valid url(s)")
+print(str(invalid_url) + " invalid url(s)\n")
 
 # Closes FTP connection
-print("closing FTP connection...")
+print("freeing system resources...")
 ftp_server.quit()
 
 # Deletes file object, freeing system resources
-print("closing URL file...")
 url_file.close()
